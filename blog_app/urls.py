5a3b89cc03e568d65316db1fee260b3bde8e270d
allan-nganga <allen.nganga7@gmail.com>
2024-11-08 08:51:08 +0300

    Added a new post button and a forms page to allow a user to add a new post

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),    # Homepage URL
+    path('post/new/', views.new_post, name='new_post'), # Add a new post URL
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all().order_by('-created_at')  #Get all posts, ordered by  creation date (newest first)
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+# Function for new post
+def new_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            # Save the post data to the database
+            post = Post(
+                title = form.cleaned_data['title'],
+                content = form.cleaned_data['content'],
+                author = request.user   # Assuming the user is logged in
+            )
+            post.save()
+            return redirect('post_list')    # Redirect to the homepage after saving
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {'form':form})
